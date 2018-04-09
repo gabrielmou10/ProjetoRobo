5c75f876f538afbd6d4a0349fa44a7a7f929e7a5
Gabrielmou <gabrielmoura98@gmail.com>
2018-04-09 16:14:23 -0300

    Modificando le_scan.py para que desvie dos objetos

--- le_scan.py
+++ le_scan.py
@@ -12,12 +12,19 @@ from sensor_msgs.msg import LaserScan
 def scaneou(dado):
 	print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
 	print("Leituras:")
-	print(np.array(dado.ranges).round(decimals=2))
-	#print("Intensities")
-	#print(np.array(dado.intensities).round(decimals=2))
+	dists = (np.array(dado.ranges).round(decimals=2))
 
-	
+	for distancia in dists[310:]:
+		if distancia < 0.5 and distancia != 0.0:
+			print("Gira pra esquerda")
+			vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, 2))
+		velocidade_saida.publish(vel)
 
+	for distancia in dists[:50]:
+		if distancia < 0.5 and distancia != 0.0:
+			print("Gira pra direita")
+			vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, -2))
+		velocidade_saida.publish(vel)
 
 if __name__=="__main__":
 
@@ -25,6 +32,7 @@ if __name__=="__main__":
 
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
 	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
+
 
 
 
