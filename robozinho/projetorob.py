96cc66269639a74741d73f529922738d8cf527c6
Gabrielmou <gabrielmoura98@gmail.com>
2018-04-13 15:02:30 -0300

    Nova Tentativa

--- robozinho/projetorob.py
+++ robozinho/projetorob.py
@@ -12,7 +12,7 @@ import cv2
 import time
 from geometry_msgs.msg import Twist, Vector3, Pose
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Image, CompressedImage
+from sensor_msgs.msg import Image, CompressedImage, LaserScan
 from cv_bridge import CvBridge, CvBridgeError
 import smach
 import smach_ros
@@ -20,9 +20,6 @@ import smach_ros
 import cormodule
 import corinthians
 import sobrevivencia
-
-coringao = False
-caixa_cor = False
 
 bridge = CvBridge()
 
@@ -47,6 +44,10 @@ check_delay = True # Só usar se os relógios ROS da Raspberry e do Linux deskto
 #Variaveis do corinthians.py
 N_MATCH_COUNT=10
 
+encontrou_algo = False 
+coringao = False 
+caixa_cor = False
+
 detector= cv2.xfeatures2d.SIFT_create()
 
 
@@ -56,6 +57,7 @@ flann=cv2.FlannBasedMatcher(flannParam,{})
 
 trainImg=cv2.imread("coringao.png",0)
 trainKP,trainDesc=detector.detectAndCompute(trainImg,None)
+
 
 
 
@@ -80,14 +82,58 @@ def roda_todo_frame(imagem):
         cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
         media, centro, area, caixa_cor = cormodule.identifica_cor(cv_image)
         media_corin, coringao = corinthians.procuracor(cv_image)
-        if media[0] == 0:
+        if media_corin[0] != 0:
             media = media_corin
         depois = time.clock()
         cv2.imshow("Camera", cv_image)
     except CvBridgeError as e:
         print('ex', e)
 
+def roda_scan(dado):
+    global dists
+    global encontrou_algo
+    print("chegoulaser")
+    dists = np.array(dado.ranges).round(decimals=2)
+    for distancia in dists[:30]:
+        if distancia == None:
+            encontrou_algo = False
 
+        elif distancia < 0.4 and distancia > 0.1:
+            print(distancia)
+            print
+            encontrou_algo = True
+            rospy.sleep(0.05);
+        else:
+            print("nao achou")
+            encontrou_algo = False
+            rospy.sleep(0.05)
+
+def scaneia(dists,velocidade_saida):
+    #print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
+    #print("Leituras:")z
+    global encontrou_algo
+    #global velocidade_saida
+    global distancia
+
+    #dists = (np.array(dado.ranges).round(decimals=2))
+    vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+
+#distâncias a frenta direita
+    for distancia in dists[330:]:
+        if distancia < 0.5 and distancia > 0.11:
+            print(distancia)
+            print("Gira pra direita")
+            encontrou_algo = True
+            vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, 1))
+
+#distâncias a frente esquerda
+    for distancia in dists[:30]:
+        if distancia < 0.5 and distancia > 0.11:
+            print(distancia)
+            print("Gira pra esquerda")
+            encontrou_algo = True
+            vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, -1))
+    velocidade_saida.publish(vel)
 
 
 
@@ -101,7 +147,6 @@ class Girando(smach.State):
         smach.State.__init__(self, outcomes=['alinhou', 'girando'])
 
     def execute(self, userdata):
-        global velocidade_saida
 
         if media is None or len(media)==0:
             return 'girando'
@@ -126,24 +171,39 @@ class Girando(smach.State):
 
 # Robo centralizando a cor com a sua visão e seguindo em direção
 
-class Centralizado(smach.State):
+class Sobrevivendo(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['alinhando', 'alinhado'])
+        smach.State.__init__(self, outcomes=['achou', 'naoachou'])
+
+    def execute(self, userdata):
+        if encontrou_algo == True:
+            print("encontrou")
+            scaneia(dists, velocidade_saida)
+            return 'achou'
+        else:
+            return 'naoachou'
+
+class Andando(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['sobrevivendo','alinhando','andando'])
 
     def execute(self, userdata):
         global velocidade_saida
 
         #Aqui verificamos qual objeto esta na tela, dando prioridade para o simbolo do corinthians
         #A velocidade muda de acordo com o objeto na tela
-        if coringao == True:
-            velocidade_reta = Vector3(1,0,0)
+        if encontrou_algo == True:
+            return 'sobrevivendo'
+        elif coringao == True:
+            velocidade_reta = Vector3(-0.4,0,0)
         elif caixa_cor == True:
-            velocidade_reta = Vector3(-1,0,0)
+            velocidade_reta = Vector3(0.4,0,0)
         else:
             velocidade_reta = Vector3(0,0,0)
+            #return 'alinhando'
 
         if media is None:
-            return 'alinhou'
+            return 'andando'
         if  math.fabs(media[0]) > math.fabs(centro[0] + tolerancia_x):
             return 'alinhando'
         if math.fabs(media[0]) < math.fabs(centro[0] - tolerancia_x):
@@ -152,7 +212,8 @@ class Centralizado(smach.State):
             vel = Twist(velocidade_reta, Vector3(0, 0, 0))
             velocidade_saida.publish(vel)
             rospy.sleep(0.5)
-            return 'alinhado'
+            return 'andando'
+
 
 # main - executa tudo
 def main():
@@ -162,8 +223,8 @@ def main():
 
     # Para usar a webcam
     #recebedor = rospy.Subscriber("/cv_camera/image_raw/compressed", CompressedImage, roda_todo_frame, queue_size=1, buff_size = 2**24)
-    recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
-
+    recebe_scan = rospy.Subscriber("/scan", LaserScan, roda_scan)
+    recebe_cam = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
     velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
     # Create a SMACH state machine
@@ -180,11 +241,12 @@ def main():
         smach.StateMachine.add('GIRANDO', Girando(),
                                 transitions={'girando': 'GIRANDO',
                                 'alinhou':'CENTRO'})
-        smach.StateMachine.add('CENTRO', Centralizado(),
+        smach.StateMachine.add('SOBREVIVE', Sobrevivendo(),
+                                transitions={'achou': 'SOBREVIVE',
+                                'naoachou':'CENTRO'})
+        smach.StateMachine.add('CENTRO', Andando(),
                                 transitions={'alinhando': 'GIRANDO',
-                                'alinhado':'CENTRO'})
-
-
+                                'andando':'CENTRO','sobrevivendo':'SOBREVIVE'})
     # Execute SMACH plan
     outcome = sm.execute()
     #rospy.spin()
--- robozinho/sobrevivencia.py
+++ robozinho/sobrevivencia.py
@@ -9,48 +9,54 @@ from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 
 
-def scaneou(dado):
+def scaneou(dado,velocidade_saida):
     #print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
     #print("Leituras:")
+    global dists
+    global encontrou_algo
+
     dists = (np.array(dado.ranges).round(decimals=2))
 
 #distâncias a frenta direita
     for distancia in dists[330:]:
         if distancia < 0.5 and distancia != 0.0:
+            print(distancia)
             print("Gira pra direita")
+            encontrou_algo = True
             vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, 1))
-        velocidade_saida.publish(vel)
 
 #distâncias a frente esquerda
     for distancia in dists[:30]:
         if distancia < 0.5 and distancia != 0.0:
+            print(distancia)
             print("Gira pra esquerda")
+            encontrou_algo = True
             vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, -1))
-        velocidade_saida.publish(vel)
 
 #distâncias a esquerda
     for distancia in dists[30:90]:
         if distancia < 0.5 and distancia != 0.0:
-            print("Gira pra esquerda")
-            vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, -3))
-        velocidade_saida.publish(vel)
+            print(distancia)
+            print("Gira pra direita forte")
+            encontrou_algo = True
+            vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, -1.5)
 
 #distâncias a direita
     for distancia in dists[270:330]:
         if distancia < 0.5 and distancia != 0.0:
-            print("Gira pra esquerda")
-            vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, 3))
-        velocidade_saida.publish(vel)
+            print(distancia)
+            print("Gira pra esquerda forte")
+            encontrou_algo = True
+            vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, 1.5))
 
 #distâncias atras
     for distancia in dists[90:270]:
         if distancia < 0.5 and distancia != 0.0:
-            print("Gira pra esquerda")
+            print(distancia)
+            print("vai pra frente")
+            encontrou_algo = True
             vel = Twist(Vector3(1, 0, 0), Vector3(0, 0,0))
-        velocidade_saida.publish(vel)
-
-
-
+    velocidade_saida.publish(vel)
 
 
 if __name__=="__main__":
